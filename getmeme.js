51ef88918c93ebd5746ec62ddcdefd0b2049eb8b
drewpearce <drewpearce@users.noreply.github.com>
2016-09-01 10:07:55 -0400

    Update getmeme.js

--- getmeme.js
+++ getmeme.js
@@ -1,4 +1,5 @@
 var lodash = require("lodash.min.js");
+var s3BaseUrl = '' //input your s3 base url here (with trailing slash)
 var params = event.request.parameters;
 var regex = new RegExp(params.text, 'gi');
 var filesList = platform.api.get("memebot?as_list=true&include_folders=false&include_files=true&full_tree=true&zip=false");
@@ -18,7 +19,7 @@ if (filesList.content.resource) {
         }
         return {
             "response_type": "in_channel",
-            "text": "https://s3.amazonaws.com/memebot/" + matchList[itemNo]
+            "text": s3BaseUrl + matchList[itemNo]
         };
     } else {
         return 'no matches';
