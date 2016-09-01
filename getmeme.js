bcbc66d9ee2b89f9989edc34a67d28ed5fea950d
drewpearce <drewpearce@users.noreply.github.com>
2016-09-01 12:09:35 -0400

    Multiple terms and Response Object
    Addresses Issues #3 & #6.

--- getmeme.js
+++ getmeme.js
@@ -1,29 +1,54 @@
-var lodash = require("lodash.min.js");
-var s3BaseUrl = '' //input your s3 base url here (with trailing slash)
+//system vars
+var s3BaseUrl = 'https://s3.amazonaws.com/memebot/' //input your s3 base url here (with trailing slash)
+
+//libraries
+var lodash = require("lodash.min.js"); //lodash each used for looping
+
+//input vars
 var params = event.request.parameters;
-var regex = new RegExp(params.text, 'gi');
+var textIn = params.text.split(' ');
+
+//data vars
 var filesList = platform.api.get("memebot?as_list=true&include_folders=false&include_files=true&full_tree=true&zip=false");
 var matchList = [];
+var termCount = 0;
+
+//response var
+var response = {};
 
 if (filesList.content.resource) {
-    lodash._.each(filesList.content.resource, function( entry ) {
-        if (entry.match(regex)) {
-            matchList.push(entry);
+    lodash._.each(textIn, function( term ) {
+        matchList[termCount] = [];
+        var regex = new RegExp(term, 'gi');
+        if (termCount === 0) {
+            lodash._.each(filesList.content.resource, function( entry ) {
+                if (entry.match(regex)) {
+                    matchList[termCount].push(entry);
+                }
+            });
+        } else {
+            lodash._.each(matchList[termCount-1], function( entry ) {
+                if (entry.match(regex)) {
+                    matchList[termCount].push(entry);
+                }
+            });
         }
+        termCount = termCount + 1;
     });
-    if (matchList.length !== 0) {
-        if (matchList.length > 1) {
-            var itemNo = Math.floor((Math.random() * matchList.length) + 0);
+    matchLength1 = matchList.length;
+    if (matchLength1 !== 0) {
+        if (matchList[matchLength1-1].length > 1) {
+            var itemNo = Math.floor((Math.random() * matchList[matchLength1-1].length) + 0);
         } else {
             var itemNo = 0;
         }
-        return {
-            "response_type": "in_channel",
-            "text": s3BaseUrl + matchList[itemNo]
-        };
+        response.response_type = 'in_channel';
+        response.text = matchList[matchLength1-1][itemNo];
     } else {
-        return 'no matches';
+        response.text = 'no matches';
     }
 } else {
-    return 'no files';
+    response.text = 'no files';
 }
+
+return response;
